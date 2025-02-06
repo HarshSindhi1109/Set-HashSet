886b62120170289a9e3520ec5fa4f4d60cfeedce
HarshSindhi1109 <harshsindhi085@gmail.com>
2025-02-06 11:50:10 +0530

    Adding HashSet.java

--- src/main/java/org/example/NewHashSet.java
+++ src/main/java/org/example/NewHashSet.java
@@ -1,4 +1,14 @@
 package org.example;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class NewHashSet {
+    public void getNewHashSet() {
+        Set set = new HashSet();
+        for (int i = 50; i > 0; i--) {
+            set.add(i);
+        }
+        System.out.println(set);
+    }
 }
