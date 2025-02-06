747f2f7f9980797bbf2f5fb14a99b084b7dd78ac
HarshSindhi1109 <harshsindhi085@gmail.com>
2025-02-06 11:27:04 +0530

    Initial Commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+            Set<String> set = new HashSet<>();
+            set.add("Harsh");
+            set.add("Souvik");
+            set.add("Gautam");
+            set.add("Sanskar");
+            set.add("Vivek");
+
+            System.out.println(set);
+
+            for (String s : set) {
+                System.out.println(s);
+            }
+        System.out.println(set.contains("Harsh"));
+    }
+}
